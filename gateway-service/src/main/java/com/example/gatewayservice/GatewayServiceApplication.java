185b0a20e95d787bdb7aec7724beebe564f3817b
ELMGHARU <aya.elmghari2003@gmail.com>
2024-12-23 14:40:27 +0100

    Initial commit

---
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,46 @@
+package com.example.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+    @Bean
+    RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/clients/**")
+                        .uri("lb://service-client"))
+                .route(r -> r.path("/voitures/**")
+                        .uri("lb://service-voiture"))
+                .build();
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("http://localhost:3000");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.addAllowedMethod("*");
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+}
